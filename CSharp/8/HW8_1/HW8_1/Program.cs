0e3c72e3dfefff864f3038c46350d16d34aaea7d
Tarasqrtv <fedorenkotv16@gmail.com>
2018-06-04 12:51:43 +0300

    Попитка 8_1

--- CSharp/8/HW8_1/HW8_1/Program.cs
+++ CSharp/8/HW8_1/HW8_1/Program.cs
@@ -8,8 +8,57 @@ namespace HW8_1
 {
 	class Program
 	{
+		static void DoWork()
+		{
+			int value = 5;
+
+			User user = new User();
+
+			int refvalue = user.RefSetValue(ref value);
+
+			user.GetFullInfo(value, refvalue);
+
+			value++;
+
+			user.GetFullInfo(value, refvalue);
+
+			value = user.SetValue(value);
+
+			int clonevalue = user.GetClone(value);
+
+			int refclonevalue = user.RefGetClone(ref value);
+
+
+
+
+
+
+			//User user = new User();
+
+			//var number = 0;
+
+			//user.SetValueType(ref number);
+			//Console.WriteLine("Value Type: " + user.Value);
+			//number++;
+			//Console.WriteLine("After Changing: " + user.Value);
+
+			//user.SetRefType(new WrappedInt() { Number = 0 });
+			//Console.WriteLine("Reference Type: " + user.RefType);
+			//user.ChangeRefType();
+			//Console.WriteLine("After Changing: " + user.RefType);
+		}
+
 		static void Main(string[] args)
 		{
+			try
+			{
+				DoWork();
+			}
+			catch (Exception ex)
+			{
+				Console.WriteLine(ex.Message);
+			}
+			Console.ReadKey();
 		}
 	}
 }
---
+++ CSharp/8/HW8_1/HW8_1/User.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HW8_1
+{
+	class User
+	{		
+		public int RefSetValue(ref int value)
+		{
+			return value;
+		}
+		public int SetValue(int value)
+		{
+			return value;
+		}
+		public int RefGetClone(ref int value)
+		{
+			return value;
+		}
+		public int GetClone(int value)
+		{
+			return value;
+		}
+		public void GetFullInfo(int v1, int v2)
+		{
+			Console.WriteLine(v1.ToString() + Environment.NewLine + v2.ToString());
+		}
+
+
+
+
+
+
+
+
+		//int _value;
+		//WrappedInt _refType;
+
+		//public int Value  { get { return _value; } }
+		//public int RefType { get { return _refType.Number; } }
+
+		//public void SetValueType(ref int value)
+		//{
+		//	_value = value;
+		//}
+
+		//public void SetRefType(WrappedInt refValue)
+		//{
+		//	_refType = refValue;
+		//}
+
+		//public void ChangeValueType()
+		//{
+		//	_value++;
+		//}
+
+		//public void ChangeRefType()
+		//{
+		//	_refType.Number++;
+		//}
+	}
+	class WrappedInt
+	{
+		public int Number;
+	}
+}
