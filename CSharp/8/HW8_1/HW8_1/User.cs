65c1f16aabb620f01c2cb1e01d37587edb488c5d
Tarasqrtv <fedorenkotv16@gmail.com>
2018-06-04 22:49:02 +0300

    8_1 almost ready

--- CSharp/8/HW8_1/HW8_1/User.cs
+++ CSharp/8/HW8_1/HW8_1/User.cs
@@ -7,60 +7,32 @@ using System.Threading.Tasks;
 namespace HW8_1
 {
 	class User
-	{		
-		public int RefSetValue(ref int value)
+	{
+		int _value;
+		WrappedInt _refType;
+
+		public int Value { get { return _value; } }
+		public int RefType { get { return _refType.Number; } }
+
+		public void SetValueType(ref int value)
 		{
-			return value;
-		}
-		public int SetValue(int value)
-		{
-			return value;
-		}
-		public int RefGetClone(ref int value)
-		{
-			return value;
-		}
-		public int GetClone(int value)
-		{
-			return value;
-		}
-		public void GetFullInfo(int v1, int v2)
-		{
-			Console.WriteLine(v1.ToString() + Environment.NewLine + v2.ToString());
+			_value = value;
 		}
 
+		public void SetRefType(WrappedInt refValue)
+		{
+			_refType = refValue;
+		}
 
+		public void ChangeValueType()
+		{
+			_value++;
+		}
 
-
-
-
-
-
-		//int _value;
-		//WrappedInt _refType;
-
-		//public int Value  { get { return _value; } }
-		//public int RefType { get { return _refType.Number; } }
-
-		//public void SetValueType(ref int value)
-		//{
-		//	_value = value;
-		//}
-
-		//public void SetRefType(WrappedInt refValue)
-		//{
-		//	_refType = refValue;
-		//}
-
-		//public void ChangeValueType()
-		//{
-		//	_value++;
-		//}
-
-		//public void ChangeRefType()
-		//{
-		//	_refType.Number++;
-		//}
+		public void ChangeRefType()
+		{
+			_refType.Number++;
+		}
 	}
 	class WrappedInt
 	{
