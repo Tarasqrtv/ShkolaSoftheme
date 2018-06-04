e0413fea4bd8be29511590a2f03384319edd9f84
Tarasqrtv <fedorenkotv16@gmail.com>
2018-06-05 01:56:13 +0300

    Delete other files, and job Home Task

---
+++ FrontEnd/internship2018-1/src/user.service.js
@@ -0,0 +1,12 @@
+import { user } from './user.js';
+function showMessage() {
+    alert('Congratulations to you! This application was created intern Fedorenko!');
+}
+export function confirmAge(){
+    var messege = prompt("What's your age?", "Enter your age");
+    if(messege>="18"){
+        showMessage();
+    }else{
+        window.location.href='https://www.youtube.com/watch?v=OK03o3BHafk';        
+    }
+}
