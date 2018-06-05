71882031f32e1d12b0be5a36a1a5622c8b31e358
Tarasqrtv <fedorenkotv16@gmail.com>
2018-06-05 17:10:51 +0300

    Fixed an error (ABC == 18)

--- FrontEnd/internship2018-1/src/user.service.js
+++ FrontEnd/internship2018-1/src/user.service.js
@@ -2,11 +2,11 @@ import { user } from './user.js';
 function showMessage() {
     alert('Congratulations to you! This application was created intern Fedorenko!');
 }
-export function confirmAge(){
-    var messege = prompt("What's your age?", "Enter your age");
-    if(messege>="18"){
+export function confirmAge() {
+    var messege = parseInt(prompt("What's your age?", "Enter your age"));
+    if (messege >= 18) {
         showMessage();
-    }else{
-        window.location.href='https://www.youtube.com/watch?v=OK03o3BHafk';        
+    } else {
+        window.location.href = 'https://www.youtube.com/watch?v=OK03o3BHafk';
     }
 }
