297575de04ef8752f1e584f06aa4ebc5a2f273b4
Tarasqrtv <fedorenkotv16@gmail.com>
2018-06-11 00:12:21 +0300

    Task 14

---
+++ CSharp/14/HW14/HW14/Lottery.cs
@@ -0,0 +1,61 @@
+﻿using System;
+
+namespace HW14
+{
+	class Lottery
+	{
+		private int[] _values;
+
+		public int this[int index]
+		{
+			get { return _values[index]; }
+			set { _values[index] = value; }
+		}
+
+		public Lottery(int length)
+		{
+			_values = new int[length];
+
+			Random rnd = new Random();
+
+			for (int i = 0; i < length; i++)
+			{
+				_values[i] = rnd.Next(1, 9);
+			}
+		}
+
+		public int Length
+		{
+			get { return _values.Length; }
+		}
+
+		public bool СompareValues(int[] values)
+		{
+			if (this.Length != values.Length)
+			{
+				return false;
+			}
+
+			for (int i = 0; i < this.Length; i++)
+			{
+				if (this[i] != values[i])
+				{
+					return false;
+				}
+			}
+			return true;
+		}		
+
+		public override string ToString()
+		{
+			var str = String.Empty;
+
+			for (int i = 0; i < this.Length; i++)
+			{
+				str += " " + this[i];
+			}
+
+			return str;
+		}
+	}
+}
---
+++ CSharp/14/HW14/HW14/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace HW14
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			DoWork();
+		}
+		static void DoWork()
+		{
+			var lottery = new Lottery(6);
+
+			var values = new int[6];
+
+			var value = 0;
+
+			do
+			{
+				Console.WriteLine("Input 6 values(whith enter): ");
+
+				for (int i = 0; i < values.Length; i++)
+				{
+					if (!int.TryParse(Console.ReadLine(), out value))
+					{
+						Console.WriteLine("Input Error! Restart...");
+
+						break;
+					}
+					values[i] = value;
+				}
+				
+				if (lottery.СompareValues(values))
+				{
+					Console.WriteLine("You win!");
+				}
+				else
+				{
+					Console.WriteLine("You lose!");
+				}
+				Console.WriteLine(lottery.ToString() + " - is a bonus combination");
+			}
+			while (Console.ReadKey().Key != ConsoleKey.Escape);
+		}
+	}
+}
