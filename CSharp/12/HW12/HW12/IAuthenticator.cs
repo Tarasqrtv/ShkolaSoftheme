006198b47e7883c6a690359990f71b1dda391eca
Tarasqrtv <fedorenkotv16@gmail.com>
2018-06-18 21:31:40 +0300

    Task12

---
+++ CSharp/12/HW12/HW12/IAuthenticator.cs
@@ -0,0 +1,7 @@
+﻿namespace HW12
+{
+	interface IAuthenticator
+	{
+		void AuthenticateUser(IUser user);
+	}
+}
---
+++ CSharp/12/HW12/HW12/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace HW12
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			UserJob userJob = new UserJob();
+			userJob.UserJob1();
+			Console.ReadKey();
+		}
+	}
+}
